09fcc64b975829a42c6bf91d159977b5ee655f4b
Elaine Ou <elaine.ou@gmail.com>
2015-08-29 04:10:48 -0700

    fix search api

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import MySQLdb as db
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from query_tools import *
 from query_api import *
 import logging
@@ -14,20 +14,21 @@ app = Flask(__name__,  template_folder='client', static_folder='client/static')
 # app.config['MYSQL_DATABASE_DB'] = 'glass'
 # app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 # mysql.init_app(app)
-
+    
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    data = request.get_json()
-    url = data.get('url')
-    logging.info(url)
-    if url:
-        return search_query(url)
-    else:
-        return "error"
+    return request.data
+    #data = request.get_json()
+    #return data
+    # url = data.get('url')
+    # if url:
+    #     return search_query(url)
+    # else:
+    #     return "error"
 
 
 if __name__ == '__main__':
