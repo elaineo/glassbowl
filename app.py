d1484a5c66b84983a1f0c9870836645e3db4dcc2
Wenjie <wenjiewuchem@gmail.com>
2015-08-29 05:07:07 -0700

    test

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__,  template_folder='client', static_folder='client/static')
 # app.config['MYSQL_DATABASE_DB'] = 'glass'
 # app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 # mysql.init_app(app)
-    
+
 @app.route('/')
 def home():
     return render_template('index.html')
