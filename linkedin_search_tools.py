df54cc9a91d8d8d08b83fb977982f101381d764c
Aronima Dass <adass@apple.com>
2015-08-29 12:05:53 -0700

    queryable url

--- linkedin_search_tools.py
+++ linkedin_search_tools.py
@@ -2,13 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 
 def publish_profiles(dict):
-    url = "http://www.linkedin.com/title/"+ dict["Title"]+ "at" + dict["Company"]
+    url = "http://www.linkedin.com/title/"+ dict["Title"]+ " at " + dict["Company"]
     r = requests.get(url)
-    print r.content
-    rank = BeautifulSoup(r.content).find("div", {"class": "media-body"}).contents
-    #divs = BeautifulSoup(r.content).find('div', {'id': b})
     return rank
-
 print publish_profiles({"Company":"Oracle", "Title":"Senior Sales Executive"})
 
 
