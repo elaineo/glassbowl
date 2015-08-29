012688f0a37bb8f52b7aa0b90f72e37d48a1c22f
Aronima Dass <adass@apple.com>
2015-08-29 10:15:01 -0700

    added retreival with Company and Title

--- minidb.py
+++ minidb.py
@@ -10,3 +10,19 @@ def getInfo(index):
     dict["Title"]=data[1]
     dict["Salary"] = data[2]
     return dict
+
+def getSalary(dict_list):
+    db1 = mdb.connect(host="localhost",user="root", db="mydata")
+    cur = db1.cursor()
+    dict_out_list = []
+    for dict in dict_list:
+    	dict_out ={}
+        company = dict["Company"]
+        title = dict["Title"]
+        cur.execute("SELECT Company,Title,Salary FROM Salaries WHERE Company=%s AND Title=%s", (company, title))
+        data = cur.fetchone()
+        dict_out["Company"] = data[0]
+        dict_out["Title"]=data[1]
+        dict_out["Salary"]=data[2]
+        dict_out_list.append(dict_out)
+    return dict_out_list
