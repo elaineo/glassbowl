21a4b50341b9e56f6c20aaa0cb56b87c052e3e51
Ubuntu <ubuntu@ip-172-31-7-253.us-west-1.compute.internal>
2015-08-29 09:46:38 +0000

    added latin-1 encoder support for ubuntu

--- database.py
+++ database.py
@@ -1,8 +1,15 @@
+# -- coding: utf-8 --
+
 import json
 from pprint import pprint
 import MySQLdb as mdb
+import sys
+import codecs
 
-json_file='glassdoor_list.json'
+
+#sys.stdout.buffer.write(chr(9986).encode('utf8'))
+
+json_file='clean_glassdoor.json'
 
 #json_file='test.json'
 json_data=open(json_file)
@@ -33,8 +40,22 @@ with db1:
 	titles = company[company_name]
 	all_titles = titles.keys()
 	for title in all_titles:
-	    	#print title
+	    	print title
 	    	record = titles[title]
 	    	#print record
 	    	#print("INSERT INTO Salaries(Company) VALUES(\"" + company + "\")")
-	    	cur.execute("INSERT INTO Salaries(Company, Title, Salary) VALUES(\"" + company_name + "\", \""+ title.replace("-", "/") + "\", \"" + record["max"]+ "\")")
+		#title.replace("-", "/")
+		#title = title.replace("/\u2013|\u2014/g", "/");
+		
+		#if isinstance(title,basestring):
+    		#	title.encode('utf8')
+		#else:
+    		#	unicode(title).encode('utf8')
+	    	
+		#title.encode('utf-8')
+		#replace(u'\u2013', '-').encode('utf8')
+		title = title.encode('latin-1', errors='replace')
+		#title = title.encode('ascii', errors='replace')
+		
+		#cur.execute("INSERT INTO Salaries(Company, Title, Salary) VALUES(\"" + company_name + "\", \"" + title.replace("/", "-") +"\", \"" + record["max"]+ "\")")
+		cur.execute("INSERT INTO Salaries(Company, Title, Salary) VALUES(%s, %s, %s)", (company_name, title.replace("/", "-"), record["max"]))
