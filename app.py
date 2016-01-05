30c0592cf0ca052538ffe058bb0b5dbaaaf754dd
Elaine Ou <elaine.ou@gmail.com>
2016-01-04 18:34:06 -0800

    more cleanup

--- app.py
+++ app.py
@@ -4,25 +4,11 @@ from query_api import *
 import logging,sys
 from peer_puller import *
 
-
 app = Flask(__name__,  template_folder='client', static_folder='client/static')
 import json
 app.config['FLASK_LOG_LEVEL'] = 'DEBUG'
 logging.basicConfig(stream=sys.stderr)
 app.logger.addHandler(logging.StreamHandler(stream=sys.stderr))
-
-fakepeers = [{ "headline": "Software Engineer",
-      "img":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/4/005/042/1a1/08b48ac.jpg",
-      "url": "https://www.linkedin.com/profile/view?id=ADEAAAauYmQBGwCTxbICPCV-k53MsxyYuaGiOyw"
-      },
-      { "headline": "Software Developer",
-     "img":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAMPAAAAJDQwMmRiMGJlLWNkNTgtNDAxNC1hN2UwLTNmNjY3OGEwYmRiNA.jpg",
-    "url": "https://www.linkedin.com/profile/view?id=ADEAAAS-uHMBmmfUr8-urMQ0GGDu9odW3VYjK1w"
-  },
-  { "headline": "Software Developer",
-     "img":"http://www.utexas.edu/cola/history/_files/images/people/graduation_speakers/ryan_maidie.jpg",
-    "url": "https://www.linkedin.com/profile/view?id=ADEAAA93W-MBpZgbhfh6gD2mcvbXZmVNZ9DurXs"
-  }]
 
 # mysql = MySQL()
 
